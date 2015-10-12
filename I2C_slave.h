88a62d988c2e5538589fa5b1683ba88a879d6d63
hinagesi_708 <hinagesi-708@users.noreply.github.com>
2015-10-12 12:37:01 +0900

    Update I2C_slave.h

--- I2C_slave.h
+++ I2C_slave.h
@@ -3,8 +3,10 @@
 
 #define I2C_ADDR_DEV1    0x51
 
-signed char reseaved_data[8] = {0};
-unsigned char send_data[8] = {0};
+//signed char reseaved_data[8] = {0};
+signed char reseaved_data[1] = {0};
+//unsigned char send_data[8] = {0};
+unsigned char send_data[4] = {0};
 
 void I2C_init();
 void set_send_string(signed char Str[8]);
